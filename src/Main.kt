bae29f98785a55fcdeb2e7be5f89a1668e8e0cf7
PabloAlonsoGarcia <pabalo172@alu.centroafuera.es>
2022-10-18 12:29:37 +0200

    Ejercicio terminado

--- src/Main.kt
+++ src/Main.kt
@@ -1,64 +1,66 @@
-class Personaje(){
-    val nombre:String = "Jack"
+class Personaje() {
+    val nombre: String = "Jack"
+    val mochila = mutableListOf<Item>()
 
+//Función para robar en base al ratio de los objetos* con un peso límite
+    fun robar(objetos: ArrayList<Item>) {
+        var pesoActual: Int = 0
+        objetos.forEach{
+            if (pesoActual <= 10 && (pesoActual + it.w)<=10){
+                mochila.add(it)
+                pesoActual += it.w
+            }
+        }
 
-
-    fun robar(objetos: ArrayList<Item>){
-
+        mochila.forEach { println(it) }
 
     }
 }
 
-class Item(peso: Int= 0,valor: Int=0){
+class Item(peso: Int = 0, valor: Int = 0) {
 
-    var id=0
+
+
     var w = peso
     var v = valor
+    var ratio: Float = calculoRatio()
 
+//Asignamos a un objeto su ratio correspondiente
+    fun calculoRatio(): Float {
 
+        var num: Float = 0.0F
 
-    fun calculoRatio():Float{
-
-        var num: Float= 0.0F
-
-        num=(this.v.toFloat()/this.w.toFloat())
-
-        print(num)
+        num = (this.v.toFloat() / this.w.toFloat())
 
         return num
+
+    }
+//To string para mostrarlo
+    override fun toString(): String {
+        return "Item(w=$w, v=$v)"
     }
 
-}
 
-class Mochila(){
-    val pesoMax=10
 }
 
 
 fun main() {
 
-    var peso:Int= 0
-    var valor:Int=0
-    var pesoActual:Int=0
-
+    var peso: Int = 0
+    var valor: Int = 0
 
     val num_objetos: Int = 4
 
-    val personaje1=Personaje();
-
+    val personaje1 = Personaje();
 
     val objetos = arrayListOf<Item>()
-    val ratios = arrayListOf<Float>()
 
 
-
-//Asignamos a cada objeto un valor y un peso aleatorio
+//Asignamos a cada objeto un valor y un peso aleatorio y calculamos su ratio
     repeat(num_objetos) {
-        peso=(1..10).random()
-        valor=(10..50).random()
+        peso = (1..10).random()
+        valor = (10..50).random()
         objetos.add(Item(peso, valor))
-        objetos[it].id=it;
-        ratios.add(objetos[it].calculoRatio())
     }
 
 //Mostramos los objetos a robar
@@ -71,46 +73,14 @@ fun main() {
         print(" /valor:" + objetos[it].v)
         println("|")
     }
-    println("")
-    println("Ratios:")
-    repeat(num_objetos){
 
 
-        print("|ratio:"+ratios[it].toFloat())
-
-    }
-    println("|")
-
-//Decidimos que objetos robar
-
-    ratios.sortedDescending()
-
-    println("")
-    println("Ratios ordenados:")
-    repeat(num_objetos){
+    objetos.sortByDescending { it.ratio }
 
 
-        print("|ratio:"+ratios[it].toFloat())
-
-    }
-    println("|")
-
-
-
-
-
-
-    var cont:Int=0
-    do{
-        if(ratios[cont].equals(0) ){
-            cont++
-        }else{
-
-            pesoActual=pesoActual
-            cont++
-        }
-
-    }while(pesoActual<=10)
-
+    println()
+    personaje1.robar(objetos)
 
 }
+
+
